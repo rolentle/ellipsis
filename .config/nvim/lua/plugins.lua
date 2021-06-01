2394c8a4db1d65349883e55b2a6639b47288d637
Rolen T. Le <rolentle@gmail.com>
2021-06-01 16:39:34 -0400

    Add telescope to packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,8 @@
 return require("packer").startup(function()
   use "wbthomason/packer.nvim"
   use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+  use {
+	  'nvim-telescope/telescope.nvim',
+	  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
 end)
