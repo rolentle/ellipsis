2c00d9f17f56d2a3b4534faeb5893a4751965566
Rolen T. Le <rolentle@gmail.com>
2022-06-13 18:20:19 -0400

    comment out stuff and add elm ls

--- config/nvim/lua/ellipsis/lsp_config.lua
+++ config/nvim/lua/ellipsis/lsp_config.lua
@@ -1,44 +1,44 @@
 vim.g.completeopt='menu,menuone,noselect'
 -- Setup nvim-cmp.
-local cmp = require'cmp'
+-- local cmp = require'cmp'
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      -- For `vsnip` user.
-      -- vim.fn["vsnip#anonymous"](args.body)
+-- cmp.setup({
+--   snippet = {
+--     expand = function(args)
+--       -- For `vsnip` user.
+--       -- vim.fn["vsnip#anonymous"](args.body)
 
-      -- For `luasnip` user.
-      -- require('luasnip').lsp_expand(args.body)
+--       -- For `luasnip` user.
+--       -- require('luasnip').lsp_expand(args.body)
 
-      -- For `ultisnips` user.
-      vim.fn["UltiSnips"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+--       -- For `ultisnips` user.
+--       vim.fn["UltiSnips"](args.body)
+--     end,
+--   },
+--   mapping = {
+--     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--     ['<C-Space>'] = cmp.mapping.complete(),
+--     ['<C-e>'] = cmp.mapping.close(),
+--     ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
-  },
-  sources = {
-    { name = 'nvim_lsp' },
+--   },
+--   sources = {
+--     { name = 'nvim_lsp' },
 
-    -- For vsnip user.
-    -- { name = 'vsnip' },
+--     -- For vsnip user.
+--     -- { name = 'vsnip' },
 
-    -- For luasnip user.
-    -- { name = 'luasnip' },
+--     -- For luasnip user.
+--     -- { name = 'luasnip' },
 
-    -- For ultisnips user.
-    { name = 'UltiSnips' },
+--     -- For ultisnips user.
+--     { name = 'UltiSnips' },
 
-    { name = 'buffer' },
-    { name = 'path' },
-  }
-})
+--     { name = 'buffer' },
+--     { name = 'path' },
+--   }
+-- })
 --ruby lsp
 local nvim_lsp = require'lspconfig'
 nvim_lsp.solargraph.setup{
@@ -101,3 +101,5 @@ end
 
 -- rust lsp
 nvim_lsp.rust_analyzer.setup({})
+
+nvim_lsp.elmls.setup({root_dir = require("lspconfig.util").root_pattern('hubtran/hubtran-web/client/elm/accounts-payable/elm.json')})
